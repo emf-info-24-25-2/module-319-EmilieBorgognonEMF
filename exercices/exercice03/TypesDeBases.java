026278794780e4c796657c186976880745c0520b
EmilieBorgognonEMF <emilie.borgognon@studentfr.ch>
2024-09-05 08:59:01 +0200

    mon exercice sur les types de bases ex1

---
+++ exercices/exercice03/TypesDeBases.java
@@ -0,0 +1,10 @@
+public class TypesDeBases{
+    public static void main(String[] args) {
+        int age = 15;
+        String nom = "Emilie";
+
+        System.out.println("Je m'appelle "+ nom + " et j'ai  " +age+ "ans");
+        
+    }
+    }
+
