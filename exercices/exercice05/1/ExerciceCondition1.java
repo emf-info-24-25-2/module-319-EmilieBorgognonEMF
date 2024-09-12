20cfbde2fdc9f59e1879208a063c165c20236c37
EmilieBorgognonEMF <emilie.borgognon@studentfr.ch>
2024-09-12 11:15:27 +0200

    ExercicesConditions

---
+++ exercices/exercice05/1/ExerciceCondition1.java
@@ -0,0 +1,13 @@
+public class ExerciceCondition1 {
+    public static void main(String[] args) {
+        int valeurATester = -2;
+        if (valeurATester<0){
+            System.out.println("la valeur est négative");
+        }
+        if (valeurATester>0){
+            System.out.println("la valeur est positive");
+        }
+
+
+    }
+}
---
+++ exercices/exercice05/3/ExercicieCondidtion3.java
@@ -0,0 +1,13 @@
+public class ExercicieCondidtion3 {
+    public static void main(String[] args) {
+        int valeur1 = 4;
+        int valeur2 = 5;
+        if (valeur1 + valeur2 <0){
+            System.out.println("le résultat est négatif");
+            if (valeur1 + valeur2 >0){
+                System.out.println("le résultat est positif");
+            }
+        }
+        
+    }
+}
