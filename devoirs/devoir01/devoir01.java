2e07b962bb31c95b3452786ed4ffed3538fb0585
EmilieBorgognonEMF <emilie.borgognon@studentfr.ch>
2024-09-12 09:54:10 +0200

    devoir01

---
+++ devoirs/devoir01/devoir01.java
@@ -0,0 +1,14 @@
+public class devoir01 {
+  public static void main(String[] args) {
+    System.out.println("je m'appelle BORGOGNON emilie");
+    int age =21;
+    if (age<20){
+        System.out.println("je suis pas encore majeur.");
+    }
+    if (age>20){
+        System.out.println("je suis majeur.");
+    }
+  }
+
+    
+}
