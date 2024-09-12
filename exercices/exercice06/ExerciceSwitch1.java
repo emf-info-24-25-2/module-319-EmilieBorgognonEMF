e313760775ce77a39134498c32f5a7957ee85763
EmilieBorgognonEMF <emilie.borgognon@studentfr.ch>
2024-09-12 11:42:39 +0200

    switch

---
+++ exercices/exercice06/ExerciceSwitch1.java
@@ -0,0 +1,24 @@
+public class ExerciceSwitch1 {
+    public static void main(String[] args) {
+        int notedumodule = 5;
+        if (notedumodule = 1 ){
+            System.out.println("travail non rendu");
+            } else if (notedumodule == 2) {
+                System.out.println("Très insuffisant");
+                
+            }else if (notedumodule == 3) {
+                System.out.println("Insuffisant");
+                
+            }else if (notedumodule == 4) {
+                System.out.println("Suffisant");
+                
+            }else if (notedumodule == 5) {
+                System.out.println("BIen");
+                
+            }else if (notedumodule == 6) {
+                System.out.println("Très bien");
+                
+            }
+        }
+    }
+    
